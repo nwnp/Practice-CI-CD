8092d4acaa974b909b7098bd008ce58ff8843938
nwnp <jjw_0418@naver.com>
2023-01-01 21:39:19 +0900

    feat: ci testing

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 3000;
+  const HOST = 'localhost';
+  app.listen(PORT, () => {
+    logger.verbose(`The server is on http://${HOST}:${PORT}`);
+  });
 }
 bootstrap();
